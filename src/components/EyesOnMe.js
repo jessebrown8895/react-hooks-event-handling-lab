1de32e16ad800c8b65014c6d3681e1fd0a3c30d1
Jesse Brown <Brownjesse8895@yahoo.com>
2021-12-08 03:08:08 -0600

    Event Handling in React Lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+const EyesOnMe = () => {
+    const focus = () => {
+        console.log('Good!')
+    }
+    const onBlur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+    return (
+        <button  onFocus={focus} onBlur={onBlur}>
+            {'Eyes on me'}
+        </button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
-// Code Keypad Component Here
+import React from 'react'
+
+const Keypad = () => {
+    const changEvent = () => {
+        console.log('Entering password...')
+    }
+    return <input type='password' onChange={changEvent}></input>
+}
+
+export default Keypad
